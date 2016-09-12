be6832309467bd7ef9708b3967c583c51f813e91
Kyle Luzny <kluzny@gmail.com>
2016-09-11 20:15:45 -0500

    switched lexer spec to use subject

--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -28,86 +28,102 @@ describe ThreeStars::Lexer do
   end
 
   describe '.tables' do
+    subject { instance.tables }
+
     let(:sql) { 'select * from users' }
     it 'returns an array of the sql table' do
-      expect(instance.tables).to eq(%w(users))
+      expect(subject).to eq(%w(users))
     end
   end
 
   describe '.selectors' do
+    subject { instance.selectors }
+
     context "single selector" do
       let(:sql) { 'select id from users' }
       it 'returns an array of the sql selectors' do
-        expect(instance.selectors).to eq(%w(id))
+        expect(subject).to eq(%w(id))
       end
     end
 
     context "multiple selectors" do
       let(:sql) { 'select id,name from users' }
       it 'returns an array of the sql selectors' do
-        expect(instance.selectors).to eq(%w(id name))
+        expect(subject).to eq(%w(id name))
       end
     end
   end
 
   describe '.orders' do
+    subject { instance.orders }
+
     context 'single order' do
       let(:sql) { 'select * from users order by name asc' }
       it 'returns an array of the order clauses' do
-        expect(instance.orders).to eq([%w(name ASC)])
+        expect(subject).to eq([%w(name ASC)])
       end
     end
 
     context 'single order no direction' do
       let(:sql) { 'select * from users order by name' }
       it 'returns an array of the order clauses' do
-        expect(instance.orders).to eq([%w(name DESC)])
+        expect(subject).to eq([%w(name DESC)])
       end
     end
 
     context 'multiple orders' do
       let(:sql) { 'select * from users order by name asc, id desc' }
       it 'returns an array of the order clauses' do
-        expect(instance.orders).to eq(
-          [%w(name ASC), %w(id DESC)]
-        )
+        expect(subject).to eq([%w(name ASC), %w(id DESC)])
       end
     end
   end
 
   describe '.groups' do
+    subject { instance.groups }
+
     let(:sql) { 'select name, sum(score) from users group by name' }
     it 'returns an array of the group clauses' do
-      expect(instance.groups).to eq(%w(name))
+      expect(subject).to eq(%w(name))
     end
   end
 
   describe '.wheres' do
+    subject { instance.wheres }
+
+    context 'no wheres' do
+      let(:sql) { "select *" }
+      it 'does not throw an error' do
+        expect{ subject }.to_not raise_error
+        expect(subject).to eq []
+      end
+    end
+
     context 'single where equals' do
       let(:sql) { "select * from users where name='ada'" }
       it 'returns an array of the where clauses' do
-        expect(instance.wheres).to eq(%w(name))
+        expect(subject).to eq(%w(name))
       end
     end
 
     context 'where equals joined by ands ' do
       let(:sql) { "select * from users where name='ada' and last_name='lovelace'" }
       it 'returns an array of the where clauses' do
-        expect(instance.wheres).to eq(%w(name last_name))
+        expect(subject).to eq(%w(name last_name))
       end
     end
 
     context 'where equals joined by more ands' do
       let(:sql) { "select * from users where name='ada' and last_name='lovelace' and company='tesla'" }
       it 'returns an array of the where clauses' do
-        expect(instance.wheres).to eq(%w(name last_name company))
+        expect(subject).to eq(%w(name last_name company))
       end
     end
 
     context 'where equals joined by more ands with parenthesis' do
       let(:sql) { "select * from users where (name='ada' and last_name='lovelace') and company='tesla'" }
       it 'returns an array of the where clauses' do
-        expect(instance.wheres).to eq(%w(name last_name company))
+        expect(subject).to eq(%w(name last_name company))
       end
     end
   end
